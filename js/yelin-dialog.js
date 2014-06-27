9507b74543f08c77350dc0c25e5c01ce006b302d
linye <yelin@sohu-inc.com>
2014-06-27 16:14:59 +0800

    add $.ytips function to render all ytips staff

--- js/yelin-dialog.js
+++ js/yelin-dialog.js
@@ -509,7 +509,7 @@
         }
         function createHTML(){
             var str = '';
-            str +=    '<div class="ytips-element" style="max-width:180px;z-index: 1100;background:#000;color:#fff;border:1px solid #222;border-radius:3px;position:absolute;">'
+            str +=    '<div class="ytips-element" style="max-width:180px;z-index: 1100;background:#000;color:#fff;border:1px solid #222;border-radius:3px;position:absolute;box-shadow:2px 2px 4px #333;">'
                         + '<div class="ytips-body" style="">'
                             +'<div style="font-size:13px;padding:8px 14px;">'
                                 + '<p style="text-align:center;">'+ opt.content +'</p>'
@@ -571,5 +571,11 @@
             return obj;
         }
         return this;
-    }
+    };
+    $.ytips = function(){
+        var els = $('[data-title]');
+        return els.each(function(index, el){
+            $(el).ytips();
+        });
+    };
 })(jQuery || $);
